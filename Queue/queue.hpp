66a8a0ba83b71be00158e1f5a40f8bf10f8ea677
provo <provo@users.noreply.github.com>
2017-07-09 15:19:39 -0400

    Once again fixed the tabbing issue

--- Queue/queue.hpp
+++ Queue/queue.hpp
@@ -10,36 +10,36 @@
 // Queue node structure
 struct node
 {
-		node *next;
-		char data;
+	node *next;
+	char data;
 };
 
 // Queue class definition
 class Queue
 {
 private:
-		node *front;
-		node *rear;
-		int nodeCount;
+	node *front;
+	node *rear;
+	int nodeCount;
 public:
-		// Constructors and destructor
-		Queue(const char dat);
-		Queue();
-		~Queue();
+	// Constructors and destructor
+	Queue(const char dat);
+	Queue();
+	~Queue();
 
-		// Class methods
-		inline bool isEmpty() const;
-		int getCount() const;
-		void insert(const char dat);
-		void cutTheLine(const char dat);
-		char remove();
-		void printQueue() const;
-		void deleteQueue();
+	// Class methods
+	inline bool isEmpty() const;
+	int getCount() const;
+	void insert(const char dat);
+	void cutTheLine(const char dat);
+	char remove();
+	void printQueue() const;
+	void deleteQueue();
 
-		// Overloaded operators
-		friend std::ostream & operator<<(std::ostream &os, const Queue &obj);
-		bool operator<(const Queue &obj) const;
-		bool operator>(const Queue &obj) const;
+	// Overloaded operators
+	friend std::ostream & operator<<(std::ostream &os, const Queue &obj);
+	bool operator<(const Queue &obj) const;
+	bool operator>(const Queue &obj) const;
 };
 
 
